b61157a58086adb5b60c5078d4646df653be786f
Lauty Cortez <lautycortez725@gmail.com>
2024-09-05 14:32:56 -0300

    Primer Commit

---
+++ src/app/horario.model.ts
@@ -0,0 +1,10 @@
+export interface Horario {
+    dia: string;
+    horas: string[];
+}
+
+export const DIAS_SEMANA: string[] = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'];
+export const HORAS: string[] = [
+    '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM',
+    '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', '8:00 PM', '9:00 PM', '10:00 PM', '11:00 PM', '12:00 AM'
+];
